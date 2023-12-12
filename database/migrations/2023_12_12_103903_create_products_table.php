2e90535917aec2a849524fa049a4b47de0351fbd
rodionmaksimenko <asvrodya@gmail.com>
2023-12-12 13:04:49 +0200

    Init Laravel | Created basic entities (Models, Controllers and migrations)

---
+++ database/migrations/2023_12_12_103903_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('title');
+            $table->decimal('price');
+
+            $table->string('provider_id');
+            $table->string('storage_id');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
